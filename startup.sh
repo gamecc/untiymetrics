ed8b9461c55fd329e4b9ff644c4064b5e2aa71a2
zaturday <fzo.ton@gmail.com>
2022-03-17 15:14:57 +0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+set -e
 
-if [ -f /firstrun ]; then
-systemctl status telegraph
-systemctl start telegraph
-systemctl status telegraph
+if [ "${1:0:1}" = '-' ]; then
+    set -- telegraf "$@"
 fi
 
-# Start supervisor to start the services
+if [ $EUID -ne 0 ]; then
+    exec "$@"
+else
+    # Allow telegraf to send ICMP packets and bind to privliged ports
+    setcap cap_net_raw,cap_net_bind_service+ep /usr/bin/telegraf || echo "Failed to set additional capabilities on /usr/bin/telegraf"
+
+    exec setpriv --reuid telegraf --init-groups "$@"
+fi
